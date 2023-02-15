e384fc41f6752dddf81aa34b07fe9c3cac672008
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-02-15 23:47:36 +0530

    Box-shadow generator done

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import React,{useState} from 'react';
 import './App.css';
 
 function App() {
-  const[Hori,setHori] = useState(10)
+  const[Hor,setHor] = useState(10)
   const[Veri,setVeri] = useState(10)
   const[Blur,setBlur] = useState(10)
+  const[color,setColor] = useState("Black")
+  const[CheckOn,setCheckOn] = useState(false)
   return (
     <div className="App">
     <div className="controls">
       <label>Horizontal length</label>
-        <input type="range" min="-200" max="200" value={Hori} onChange={(e)=>setHori(e.target.value)} />
+        <input type="range" min="-200" max="200" value={Hor} onChange={(e)=>setHor(e.target.value)} />
       <label>Vertical length</label>
         <input type="range" min="-200" max="200" value={Veri} onChange={(e)=>setVeri(e.target.value)} />
       <label>Blur</label>
         <input type="range" min="0" max="200" value={Blur} onChange={(e)=>setBlur(e.target.value)} />
+        <label>Color</label>
+        <input type="color" value={color} onChange={(e)=>setColor(e.target.value)} />
+        <div className="switch">
+        <label>
+          Outline
+          <input type="checkbox" checked={CheckOn} onChange={()=>setCheckOn(!CheckOn)}/>
+          <span className="lever"></span>
+          inset
+        </label>
+  </div>
 
     </div>
   <div className="output">
-    <div className="box" style={{boxSha
-    :`${Hori}px ${Veri}px ${Blur}px`}}></div>
-  </div>
+    <div className="box" style={{boxShadow:`${CheckOn?"inset":""} ${Hor}px ${Veri}px ${Blur}px ${color}` }}>
+      <p>Box-Shadow:{Hor}px {Veri}px {Blur}px {color}</p>
+    </div>
+    </div>
     </div>
   );
 }
